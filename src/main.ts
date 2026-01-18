d0b5a01527adb14d0fdb21a5ae1d40866f09f1e6
Zaki Uddin <zakiuddin810@gmail.com>
2026-01-18 22:03:22 +0530

    feat: add Swagger API docs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import { AppModule } from './app.module';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -16,6 +18,24 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('PMS API')
+    .setDescription('PMS')
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+      pageTitle: 'PMS API Reference',
+      theme: 'default',
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
