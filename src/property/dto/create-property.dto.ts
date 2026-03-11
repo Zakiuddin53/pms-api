18397e3fefc75620b39c1c3357708259fef4adc4
Zaki Uddin <zakiuddin810@gmail.com>
2026-03-12 02:35:53 +0530

    Update create-property.dto.ts

--- src/property/dto/create-property.dto.ts
+++ src/property/dto/create-property.dto.ts
@@ -9,71 +9,87 @@ import {
   ValidateNested,
 } from 'class-validator';
 import { PropertyType } from '@/common/enums/property-type.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 class CreatePropertyAboutDto {
   @IsString()
   @IsOptional()
+  @ApiProperty()
   description?: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   checkInTime?: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   checkOutTime?: string;
 }
 
 class CreatePropertyContactDto {
   @IsString()
   @IsOptional()
+  @ApiProperty()
   phone?: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   whatsapp?: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   email?: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   googleMapUrl?: string;
 }
 
 export class CreatePropertyDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   address: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   pinCode: string;
 
   @IsEnum(PropertyType)
   @IsNotEmpty()
+  @ApiProperty()
   propertyType: PropertyType;
 
   @IsOptional()
   @IsString()
+  @ApiProperty()
   city?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty()
   state?: string;
 
   @IsOptional()
   @ValidateNested()
   @Type(() => CreatePropertyContactDto)
+  @ApiProperty()
   contact?: CreatePropertyContactDto;
 
   @IsOptional()
   @ValidateNested()
   @Type(() => CreatePropertyAboutDto)
+  @ApiProperty()
   about?: CreatePropertyAboutDto;
 }
